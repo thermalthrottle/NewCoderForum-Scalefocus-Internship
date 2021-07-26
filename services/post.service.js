8e3b69b83f84c1f16c5c8325473c949f5c470be2
thermalthrottle <biz.mitko@gmail.com>
2021-07-26 22:34:39 +0300

    added services

---
+++ services/post.service.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+import Posts from '../db/posts.js';
+
+export default class postService{
+
+    constructor(){
+    }
+
+    static getAllPosts = async () => {
+        return await Posts.find();
+    }
+
+    static savePost = async (postToSave) => {
+        return await postToSave.save();
+    }
+
+
+}
